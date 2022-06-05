888950f35fedccdd1283fd3941a0744c085263d2
anguianoo <njanguiano@gmail.com>
2022-06-05 13:15:12 -0700

    done

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
-import Button from "./Button";
+import React from "react"
 
-// Whichever button was clicked should have a class of selected.
-// The other buttons should not have any class assigned.
-// make a state within each button, therefore make Button Component
+function CategoryFilter({ categories, selectedCategory, onSelectCategory }) {
+  const categoryButtons = categories.map(category => {
+    const className = category === selectedCategory ? "selected" : null
+    return (
+      <button
+        key={category}
+        className={className}
+        onClick={() => onSelectCategory(category)}
+      >
+        {category}
+      </button>
+    )
+  })
 
-function CategoryFilter({ categories }) {
-  const btnCategories = categories.map((item) => (
-    <Button key={item} item={item} children={item}>
-      {item}
-    </Button>
-  ));
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {btnCategories}
+      {categoryButtons}
     </div>
-  );
+  )
 }
 
-export default CategoryFilter;
+export default CategoryFilter
